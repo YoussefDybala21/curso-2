813396724c08b81bdf32dd7192b900278e301651
YoussefDybala21 <youssef.vieira@escola.pr.gov.br>
2026-08-28 08:11:24 -0300

    hdjdsfsj

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const botao = document.querySelector("button");
-botao.addEventListener("click",botaoClicado);
+botao.addEventListener("click", botaoClicado);
 
 function botaoClicado() {
-    let texto = botao.querySelector("span");
-    texto.textContent++;
+let texto = botao.querySelector("span");
+texto.textContent++;
 }
